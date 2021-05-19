a8acf49cf2cef2de411a825755ca76ebafba9a42
vitormarco <vitoradrianomarco@gmail.com>
2021-05-19 08:54:50 -0300

    Validate if account already exists

--- src/index.js
+++ src/index.js
@@ -10,10 +10,19 @@ const customers = [];
 app.post("/account", (req, res) => {
     const { cpf, name } = req.body;
 
-    const id = uuidv4();
+    // It should not be able to create an account with the same cpf
+    const accountAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf
+    );
+
+    if (accountAlreadyExist) {
+        return res.status(400).json({
+            error: "The account already exists."
+        });
+    }
 
     customers.push({
-        id,
+        id: uuidv4(),
         name,
         cpf,
         statement: []
